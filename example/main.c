16c9a02a488dd3d41414e4b6f65df2542b611a9d
Colleague Riley <ColleagueRiley@gmail.com>
2023-08-28 21:54:58 -0400

    added japanese and unicode examples

--- example/main.c
+++ example/main.c
@@ -46,7 +46,8 @@ int main(int argc, char **argv) {
     #endif
 
     RFont_init(win->w, win->h);
-    RFont_font* font = RFont_font_init("/usr/share/fonts/TTF/DejaVuSans.ttf");
+    RFont_font* font = RFont_font_init("DejaVuSans.ttf");
+    RFont_font* japanese = RFont_font_init("DroidSansJapanese.ttf");
 
     bool running = true;
 
@@ -62,12 +63,14 @@ int main(int argc, char **argv) {
         glClear(GL_COLOR_BUFFER_BIT);
         
         glColor4ub(255, 0, 0, 255);
-        RFont_draw_text(font, "abcdefghijklmnopqrstuvwxyz\n1234567890@.<>,/?\\|[{]}", 0, 100, 60);
-        RFont_draw_text(font, "`~!#$%^&*()_-=+", 0, 220, 60);
-        RFont_draw_text(font, "ABCDEFGHIJKLMNOPQRSTUVWXYZ", 0, 320, 60);
 
-        RFont_draw_text(font, "RFont_draw_text();", 0, 400, 60);
+        RFont_draw_text(font, "abcdefghijklmnopqrstuvwxyz\n1234567890@.<>,/?\\|[{]}", 0, 0, 60);
+        RFont_draw_text(font, "`~!#$%^&*()_-=+", 0, 120, 60);
+        RFont_draw_text(font, "ABCDEFGHIJKLMNOPQRSTUVWXYZ\nSomething about a fast lazy dog.", 0, 210, 20);
 
+        RFont_draw_text(font, "RFont_draw_text(); ⌫", 0, 240, 60);
+        RFont_draw_text(japanese, "テキスト例", 0, 300, 60);
+        
         #if defined(RFONT_RENDER_RLGL)
         rlDrawRenderBatchActive();      // Update and draw internal render batch
         #endif
@@ -81,6 +84,8 @@ int main(int argc, char **argv) {
         #endif
     }
 
+    RFont_font_free(font);
+    RFont_font_free(japanese);
 
     #if defined(RFONT_RENDER_RLGL) && !defined(RFONT_RENDER_LEGACY)
     rlglClose();
