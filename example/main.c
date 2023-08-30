b7fa030aa5c7f702a140f54bfdf03ddbdc6194d6
Colleague Riley <ColleagueRiley@gmail.com>
2023-08-30 00:10:16 -0400

    updated example code and makefile

--- example/main.c
+++ example/main.c
@@ -2,10 +2,6 @@
 #define RFONT_IMPLEMENTATION
 #define RLGL_IMPLEMENTATION
 
-#if !defined(RFONT_RENDER_LEGACY) && !defined(RFONT_RENDER_RLGL)
-#define GL_GLEXT_PROTOTYPES
-#endif
-
 #ifdef RFONT_RENDER_LEGACY
 #define GRAPHICS_API_OPENGL_11
 #endif
@@ -14,25 +10,34 @@
 #include "rlgl.h"
 #endif
 
-#if !defined(RFONT_RENDER_LEGACY) && !defined(RFONT_RENDER_RLGL) && defined(_WIN32)
+#if !defined(RFONT_RENDER_LEGACY) && !defined(RFONT_RENDER_RLGL)
 #define GLAD_MALLOC malloc
 #define GLAD_FREE free
 
 #define GLAD_GL_IMPLEMENTATION
-#include "glad.h"
+
+#include "ext/glad.h"
 #endif
 
+#include <GL/gl.h>
+
 #include "RFont.h"
+
 #include "RGFW.h"
 
-
 #include <stdbool.h>
+
+struct timeval GetTimeStamp() {
+    struct timeval tv;
+    gettimeofday(&tv,NULL);
+    return tv;
+}
 
 int main(int argc, char **argv) {
     RGFW_setGLVersion(3, 3);
     
     RGFW_window* win = RGFW_createWindow((argc > 1) ? argv[1] : "window", 200, 200, 1000, 500, 0);
-
+    
     #if defined(RFONT_RENDER_RLGL) && !defined(RFONT_RENDER_LEGACY)
     rlLoadExtensions((void*)RGFW_getProcAddress);        
     rlglInit(win->w, win->h);  
@@ -40,9 +45,12 @@ int main(int argc, char **argv) {
     rlDrawRenderBatchActive();      // Update and draw internal render batch
     #endif
 
-    #if !defined(RFONT_RENDER_LEGACY) && !defined(RFONT_RENDER_RLGL) && defined(_WIN32)
+    #if !defined(RFONT_RENDER_LEGACY) && !defined(RFONT_RENDER_RLGL)
     gladLoadGL((GLADloadfunc)RGFW_getProcAddress);
     #endif
+
+    glClearColor(0.3f, 0.3f, 0.32f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
 
     RFont_init(win->w, win->h);
     RFont_font* font = RFont_font_init("DejaVuSans.ttf");
@@ -58,14 +66,10 @@ int main(int argc, char **argv) {
             }
         }
 
-        glClearColor(100, 100, 100, 255);
+        glClearColor(0.3f, 0.3f, 0.32f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
         
-        #ifdef RFONT_RENDER_RLGL
-        rlColor4ub(255, 0, 0, 255);
-        #else
-        glColor4f(1.0f, 0, 0, 1.0f);
-        #endif
+        RFont_set_color(0.0f, 1.0f, 0, 1.0f);
 
         RFont_draw_text(font, "abcdefghijklmnopqrstuvwxyz\n1234567890@.<>,/?\\|[{]}", 0, 0, 60);
         RFont_draw_text(font, "`~!#$%^&*()_-=+", 0, 120, 60);
@@ -77,7 +81,6 @@ int main(int argc, char **argv) {
         #if defined(RFONT_RENDER_RLGL)
         rlDrawRenderBatchActive();      // Update and draw internal render batch
         #endif
-
 
         RGFW_window_swapBuffers(win);
         
