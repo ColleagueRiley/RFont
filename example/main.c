5a6a4595379bf882af543b7b493b5120e0a55fa4
ColleagueRiley <ColleagueRiley@gmail.com>
2025-01-02 15:48:55 -0500

    update RFont, add fallbacks, update license year

--- example/main.c
+++ example/main.c
@@ -26,11 +26,32 @@
 
 #include "ext/rglLoad.h"
 #endif
-
 #include "RFont.h"
 
 
 #include <stdbool.h>
+
+RFont_font* english;
+RFont_font* japanese;
+
+RFont_glyph glyphFallback(RFont_font* font, u32 codepoint, size_t size) {
+   RFONT_UNUSED(font); RFONT_UNUSED(size);
+   RFont_glyph g;
+
+   if (font == japanese)
+      g = RFont_font_add_codepointPro(english, codepoint, size, 0);
+   else
+      g = RFont_font_add_codepointPro(japanese, codepoint, size, 0);
+   
+   if (g.codepoint != 0 && g.size != 0) {
+      printf("Codepoint found in fallback font\n");
+      return g;
+   } else {
+      printf("Codepoint %s not found in fallback font either\n", RFont_codepoint_to_utf8(codepoint));
+   }
+
+   return (RFont_glyph){0, 0, 0, 0, (RFont_font*)0, 0, 0, 0, 0, 0, 0};
+}
 
 int main(int argc, char **argv) {
     #if !defined(RFONT_RENDER_LEGACY)
@@ -54,10 +75,10 @@ int main(int argc, char **argv) {
     glClear(GL_COLOR_BUFFER_BIT);
 
     RFont_init(win->r.w, win->r.h);
-    RFont_font* font = RFont_font_init("DejaVuSans.ttf");
-    RFont_font* japanese = RFont_font_init("DroidSansJapanese.ttf");
+    english = RFont_font_init("DejaVuSans.ttf");
+    japanese = RFont_font_init("DroidSansJapanese.ttf");
 
-
+    RFont_set_glyph_fallback_callback(glyphFallback);
     glViewport(0, 0, win->r.w, win->r.h);
     
     while (RGFW_window_shouldClose(win) == 0) {
@@ -72,15 +93,14 @@ int main(int argc, char **argv) {
         glClear(GL_COLOR_BUFFER_BIT);
         
         RFont_set_color(0.0f, 1.0f, 0, 1.0f);
-
-        RFont_draw_text(font, "abcdefghijklmnopqrstuvwxyz\n1234567890@.<>,/?\\|[{]}", 0, 0, 60);
-        RFont_draw_text_spacing(font, "`~!#$%^&*()_-=+", 0, 120, 60, 1.0f);
+        RFont_draw_text(english, "abcdefghijklmnopqrstuvwxyz\n1234567890@.<>,/?\\|[{]}", 0, 0, 60);
+        RFont_draw_text_spacing(english, "`~!#$%^&*()_-=+", 0, 120, 60, 1.0f);
         RFont_set_color(1.0f, 0.0f, 0, 1.0f);
-        RFont_draw_text(font, "ABCDEFGHIJKLMNOPQRSTUVWXYZ\nSomething about a fast lazy dog.", 0, 210, 20);
+        RFont_draw_text(english, "ABCDEFGHIJKLMNOPQRSTUVWXYZ\nSomething about a fast lazy 犬.", 0, 210, 20);
         RFont_set_color(0.0f, 1.0f, 0, 1.0f);
-        RFont_draw_text(font, "RFont_draw_text(); ⌫§", 0, 240, 60);
+        RFont_draw_text(english, "RFont_draw_text(); ⌫§", 0, 240, 60);
         RFont_set_color(1.0f, 0.0f, 0, 1.0f);
-        RFont_draw_text(japanese, "テキスト例", 0, 300, 60);
+        RFont_draw_text(japanese, "テキスト例hola", 0, 300, 60);
         
         #if defined(RFONT_RENDER_RGL)
         rglRenderBatch();      // Update and draw internal render batch
@@ -91,7 +111,7 @@ int main(int argc, char **argv) {
         RFont_update_framebuffer(win->r.w, win->r.h);
     }
 
-    RFont_font_free(font);
+    RFont_font_free(english);
     RFont_font_free(japanese);
 
     #if defined(RFONT_RENDER_RGL) && !defined(RFONT_RENDER_LEGACY)
